c52d05a6455ab88ef25813218ecd1bf090132c3c
Jorge Daniel <jorgedanielriveroc@gmail.com>
2025-06-29 14:32:21 -0300

    More styling adjustments

--- react-project-coder/src/App.jsx
+++ react-project-coder/src/App.jsx
@@ -5,14 +5,22 @@ import ItemListContainer from './components/ItemListContainer';
 const products = [
   { id: "1", band: "Slayer", title: "Reign in Blood", price: 25, image:"Slayer-Reign-In-Blood.jpg" },
   { id: "2", band: "Metallica", title: "Master of Puppets", price: 30, image:"metallica-master-of-puppets.webp" },
-  { id: "3", band: "Sepultura", title: "Quadra", price: 28, image:"sepultura-quadra.jpg" }
+  { id: "3", band: "Sepultura", title: "Quadra", price: 28, image:"sepultura-quadra.jpg" },
+  { id: "4", band: "Megadeth", title: "Rust in Peace", price: 40, image:"megadeth-rust-in-peace.webp" },
+  { id: "5", band: "Sepultura", title: "Roots Bloody Roots", price: 45, image:"sepultura-roots-bloody-roots.jpeg" },
+  { id: "6", band: "Slayer", title: "Show No Mercy", price: 50, image:"slayer-show-no-mercy.jpeg" },
+  { id: "7", band: "Metallica", title: "Black Album", price: 35, image:"metallica-black-album.jpeg" },
+  { id: "8", band: "Slayer", title: "God Hates Us All", price: 35, image:"slayer-god-hates-us-all.jpeg" },
+  { id: "9", band: "Metallica", title: "Kill em All", price: 40, image:"metallica-kill-em-all.jpeg" }
+
 ];
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting="Welcome to the most brutal metal record store!" products={products}/>
+      <ItemListContainer greeting={<img className="d-block mx-auto greeting" src="/CreaTuLanding-Rivero/img/greeting.png" alt="greeting" />} products={products} />
+
     </>
   );
 };
--- react-project-coder/src/components/ProductCard.jsx
+++ react-project-coder/src/components/ProductCard.jsx
@@ -5,8 +5,9 @@ const ProductCard = ({ band, title, price, image }) => {
   return (
     <div className="product-card d-flex flex-column align-items-center">
       <img src={`${import.meta.env.BASE_URL}img/${image}`} alt={`${band} - ${title}`} />
-      <h3>{band} - {title}</h3>
-      <p>Price: ${price}</p>
+      <h3 className='text-white fs-1'>{band} - {title}</h3>
+      <p className='text-white fs-2'>Price: ${price}</p>
+      <button>Add to Cart</button>
     </div>
   );
 };
